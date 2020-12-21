27dffdc1d9af3a9408be67aa105fcb76fd5cb7c0
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-12-21 03:05:34 -0700

    chore: Tutorial complete with Spyder

--- viz_example.py
+++ viz_example.py
@@ -1,3 +1,12 @@
 # Making Data Into Something You Can See - PluralSight tutorial 
 
 
+import pygal
+
+bar = pygal.Bar()
+bar.title = "Cyborg Kills"
+bar.add('Alita', 3)
+bar.add('Major', 4)
+bar.add('Sarah', 100)
+
+bar.render_in_browser()
